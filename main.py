54ae1d1d3fecb756ed5a350982fb35d47c2170f8
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-09-21 18:45:39 +0400

    real pendulum physics working w/o cart interaction

--- main.py
+++ main.py
@@ -17,52 +17,44 @@ class Game(Window):
 
             self.moves = [False, False]
 
-        def update(self):
+        def update(self, dt):
             if (self.moves[0] and self.moves[1]) or (not self.moves[0] and not self.moves[1]):
                 self.acceleration = 0
             elif self.moves[0]:
-                self.acceleration = -0.5
+                self.acceleration = -2
             else:
-                self.acceleration = 0.5
+                self.acceleration = 2
 
-            self.vel += self.acceleration
+            self.vel += self.acceleration * dt
             self.pos.x += self.vel
-            self.vel *= 0.95
+            self.vel *= 0.90
 
     class Pendulum:
         def __init__(self, winSize: tuple[int, int], mass: int, length: float) -> None:
             self.pos = Vector2d(winSize[0] / 2, winSize[1] / 2)
             self.mass = 0.1
 
-            self.angle = pi
+            self.angle = pi * 0.99
             self.angularVelocity = 0
             self.angularAcceleration = 0
             self.length = length
     
         def update(self, cartAcceleration, dt):
-            g = 9.81  # gravity
+            g = 3  # gravity
             
-            # Calculate the torque (simplified for small angles)
-            torque = -self.mass * g * self.length * sin(self.angle) + cartAcceleration * self.length
-            
-            # Moment of inertia (for a point mass at the end of a rod)
-            I = self.mass * self.length ** 2
-            
-            # Angular acceleration (from Newton's second law of rotation)
-            self.angularAcceleration = torque / I
-            
-            # Update angular velocity and angle
-            self.angularVelocity += self.angularAcceleration * dt
-            self.angle += self.angularVelocity * dt
+            # non-newtonion try:  self.angularAcceleration = sin(pi/2 - pi/4 - self.angle) * g
+            self.angularAcceleration = -g * sin(self.angle) / self.length
 
-            self.angularVelocity*=0.98
+            self.angularVelocity += self.angularAcceleration * dt
+            self.angle += self.angularVelocity
+            self.angularVelocity *= 0.98
 
 
     def __init__(self, winSize: tuple[int, int] | str = (1000, 700), title = 'Window', backgroundColor: tuple[int, int, int] = (20, 20, 20)) -> None:
         super().__init__(winSize, title, backgroundColor)
 
         self.fps = 60
-        self.fpsReference = 60
+        self.fpsReference = 36
 
         self.pos = [0, 0]
 
@@ -88,13 +80,14 @@ class Game(Window):
 
 
     def update(self, dt: float):
-        self.cart.update()
-        self.pendulum.update(self.cart.acceleration, dt*20)
+        weightedDt = dt * self.fpsReference
+        self.cart.update(weightedDt)
+        self.pendulum.update(self.cart.acceleration, weightedDt)
 
     def draw(self):
         pygame.draw.line(self.screen, (255, 255, 255), (0, self.winSize[1] / 2), (self.winSize[0], self.winSize[1] / 2))
         pygame.draw.rect(self.screen, (200, 0, 0), pygame.rect.Rect(self.cart.pos.x-25, self.cart.pos.y-12.5, 50, 25))
-        pygame.draw.line(self.screen, (255, 255, 255), (self.cart.pos.x, self.cart.pos.y), (self.cart.pos.x + cos(self.pendulum.angle+pi/2)*self.pendulum.length, self.cart.pos.y + sin(self.pendulum.angle+pi/2)*self.pendulum.length), 3)
+        pygame.draw.line(self.screen, (255, 255, 255), (self.cart.pos.x, self.cart.pos.y), (self.cart.pos.x + cos(self.pendulum.angle + pi/2)*self.pendulum.length, self.cart.pos.y + sin(self.pendulum.angle + pi/2)*self.pendulum.length), 3)
     
 a = Game()
 a.run()
